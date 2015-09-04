9921d0ff3328b7405db2e5d3fc4b4fdf189de37f
Jayshree Anandakumar <jayshree@keepworks.com>
2015-09-04 19:34:05 +0530

    Generate stars

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+
+  public GameObject star;
+
+	// Use this for initialization
+	void Start () {
+	 InvokeRepeating("CreateStar", 1f, 1.5f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+  void CreateStar(){
+    Instantiate(star);
+  }
+}
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+  public Vector2 velocity = new Vector2(-2,0);
+  public float range = 2;
+
+	// Update is called once per frame
+	void Update () {
+	  GetComponent<Rigidbody2D>().velocity = velocity;
+    transform.position = new Vector3(transform.position.x, transform.position.y - range * Random.value, transform.position.z);
+	}
+}
